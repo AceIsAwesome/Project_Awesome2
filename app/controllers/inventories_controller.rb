a47533327509f2ed9e10708e33ac7f5747b4f74a
Rachel Johnson <strmj02@moravian.edu>
2013-04-11 15:21:42 -0400

    ajhdkga

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -2,8 +2,6 @@ class InventoriesController < ApplicationController
  
   def show
     @inventory = Inventory.find(session[:user_id])
-    
-    @ingredients = @inventory.ingredient.each
   end
 
   def edit
